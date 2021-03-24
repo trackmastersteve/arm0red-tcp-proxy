f8ff7000ab841337b273607d43736e1b68ed4df4
Stephen Harris <trackmastersteve@gmail.com>
2021-03-24 12:50:20 -0700

    bug fixes

--- atp.py
+++ atp.py
@@ -1,4 +1,4 @@
-#!/bin/python
+#!/usr/bin/env python3
 #
 # atp.py
 
@@ -105,20 +105,20 @@ def receive_from(connection):
     # We set a 2 secod timeout; depending on your target, this may need to be adjusted.
     connection.settimeout(2)
 
-        try:
-            # Keep reading into the buffer until there's no more data or we time out.
-            while True:
-                data = connection.recv(4096)
+    try:
+        # Keep reading into the buffer until there's no more data or we time out.
+        while True:
+            data = connection.recv(4096)
 
-                if not data:
-                    break
+            if not data:
+                break
 
-                buffer += data
+            buffer += data
 
-        except:
+    except:
         pass
 
-        return buffer
+    return buffer
 
 # Modify any requests destined for the remote host.
 def request_handler(buffer):
