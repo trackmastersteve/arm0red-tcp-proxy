871e8258599efb36d5dbbbdeaad02c142ecbe3af
Stephen Harris <trackmastersteve@gmail.com>
2021-03-24 12:20:17 -0700

    bug fixes

--- atp.py
+++ atp.py
@@ -11,31 +11,32 @@ def server_loop(local_host,local_port,remote_host,remote_port,receive_first):
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
-        server.bind((local_host,local_port)
-    except:
+        server.bind((local_host,local_port))
+    except Exception as loopex:
+        print(loopex)
         print("[!!] Failed to listen on %s:%d" % (local_host,local_port))
-        print("[!!] Check for other listening sockets or corect permissions.")
+        print("[!!] Check for other listening sockets or correct permissions.")
         sys.exit(0)
 
-        print("[*] Listening on %s:5d" % (local_host,local_port))
+    print("[*] Listening on %s:%d" % (local_host,local_port))
 
-        server.listen(5)
+    server.listen(5)
 
-        while True:
-            client_socket, addr = server.accept()
+    while True:
+        client_socket, addr = server.accept()
 
-            # print out the local connection information
-            print("[==>] Received incoming connection from %s:%d" % (addr[0],addr[1]))
+        # print out the local connection information
+        print("[==>] Received incoming connection from %s:%d" % (addr[0],addr[1]))
 
-            # start a thread to talk to he remote host 
-            proxy_thread = threading.Thread(target=proxy_handler,args=(client_socket,remote_host,remote_port,receive_first))
+        # start a thread to talk to he remote host 
+        proxy_thread = threading.Thread(target=proxy_handler,args=(client_socket,remote_host,remote_port,receive_first))
 
-            proxy_thread.start()
+        proxy_thread.start()
 
 def proxy_handler(client_socket, remote_host, remote_port, receive_first):
     # connect to the remote host
     remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    remote_soket.connect((remote_host,remote_port))
+    remote_socket.connect((remote_host,remote_port))
 
     # receive data from the remote end if necessary
     if receive_first:
