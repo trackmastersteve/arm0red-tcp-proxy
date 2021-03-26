cb026a19900f12ae52751c51f64380f4b3a61889
Stephen Harris <trackmastersteve@gmail.com>
2021-03-25 19:10:10 -0700

    bug fixes

--- atp.py
+++ atp.py
@@ -89,15 +89,15 @@ def proxy_handler(client_socket, remote_host, remote_port, receive_first):
 
 def hexdump(src, length=16):
     result = []
-    digits = 4 if isinstance(src, inicode) else 2
+    digits = 4 if isinstance(src, unicode) else 2
     
     for i in range(0, len(src), length):
         s = src[i:i+length]
-        hexa = b' '.join(["%0*X" % (digits, ord(x)) for x in s])
-        text = b''.join([x if 0x20 <= ord(x) < 0x7F else b'.' for x in s])
-        result.append( b"%04X  %-*s  %s" % (i, length*(digits +1), hexa, text))
+        hexa = bytes(' ').join(["%0*X" % (digits, ord(x)) for x in s])
+        text = bytes('').join([x if 0x20 <= ord(x) < 0x7F else bytes('.') for x in s])
+        result.append(bytes("%04X  %-*s  %s") % (i, length*(digits +1), hexa, text))
     
-    print(b'\n'.join(result))
+    print(bytes('\n'.join(result)))
 
 def receive_from(connection):
     buffer = ""
